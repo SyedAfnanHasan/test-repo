8c6b9e3619cdeac48fed777db56ce67d70c9242f
SyedAfnanHasan <syedafnan9941@gmail.com>
2026-05-21 13:59:41 +0200

    refactor: the feature added to the main module

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-def prod_integers(num1, num2):
-    return num1 * num2
+"""Simple math function"""
+
+import double_it    # first awy to import
+# from double_it import double_it
 
 
-print(prod_integers(1, 2))
+def prod_integers(
+        num1: int,
+        num2: int
+        ) -> int:
+    """_summary_
+
+    Args:
+        num1 (dict[str, int]): _description_
+        num2 (list[int]): _description_
+
+    Returns:
+        int: _description_
+    """
+
+    x: int = num1 + num2
+
+    result = double_it.double_int(x)
+
+    return result
+
+
+if __name__ == "__main__":
+    print("inside main.py Dunder")
+    print(prod_integers(1, 2))
